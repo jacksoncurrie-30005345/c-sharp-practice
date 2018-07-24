b0613fa91d74062aafc0e09bf12312f24847d16a
Jackson Currie <30005345@student.toiohomai.ac.nz>
2018-07-25 09:52:51 +1200

    add basics and classes

---
+++ Basics/Classes/Person.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Person
+    {
+        private string name;
+        private int age;
+        private char gender;
+        private double mass;
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public int Age
+        {
+            get { return this.age; }
+            set { this.age = value; }
+        }
+
+        public char Gender
+        {
+            get { return this.gender; }
+            set { this.gender = value; }
+        }
+
+        public double Mass
+        {
+            get { return this.mass; }
+            set { this.mass = value; }
+        }
+
+        public Person(string name, int age, char gender, double mass)
+        {
+            Name = name;
+            Age = age;
+            Gender = gender;
+            Mass = mass;
+        }
+    }
+}
---
+++ Basics/Classes/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("Jackson", 19, 'm', 76.5);
+            Console.WriteLine("Name: {0}\nAge: {1}\nGender: {2}\nMass: {3}", person1.Name, person1.Age, person1.Gender, person1.Mass);
+        }
+    }
+}
