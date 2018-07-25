6e4f2abc5b95d788a7d20d61716829bcafbf5dce
Jackson Currie <30005345@student.toiohomai.ac.nz>
2018-07-26 08:28:36 +1200

    add task 7

--- Basics/Task7/Program.cs
+++ Basics/Task7/Program.cs
@@ -10,6 +10,27 @@ namespace Task7
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter first number: ");
+            int num1 = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            int num2 = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter third number: ");
+            int num3 = Int32.Parse(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                if (num1 > num3)
+                    Console.WriteLine("Largest is {0}", num1);
+                else
+                    Console.WriteLine("Largest is {0}", num3);
+            }
+            else
+            {
+                if (num2 > num3)
+                    Console.WriteLine("Largest is {0}", num2);
+                else
+                    Console.WriteLine("Largest is {0}", num3);
+            }
         }
     }
 }
