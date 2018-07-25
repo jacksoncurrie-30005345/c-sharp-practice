a7d94605d5b16d24c78fbfe963f0ffe186ee192d
Jackson Currie <30005345@student.toiohomai.ac.nz>
2018-07-26 08:32:31 +1200

    add task 8

---
+++ Basics/Task8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What was your final course mark? ");
+            int mark = Int32.Parse(Console.ReadLine());
+
+            if (mark >= 50)
+                Console.WriteLine("You passsed");
+            else
+                Console.WriteLine("You failed");
+        }
+    }
+}
