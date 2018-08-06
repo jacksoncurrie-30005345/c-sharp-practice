d77b29b71371508ab5702c59e24f908a95df951d
Jackson Currie <30005345@student.toiohomai.ac.nz>
2018-08-06 12:26:06 +1200

    add room escape game

---
+++ RoomEscape/RoomEscape/Game.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomEscape
+{
+    class Game
+    {
+        static Room[,] rooms;
+        static Room currentRoom;
+        static int xpos;
+        static int ypos;
+
+        static void Main(string[] args)
+        {
+            // Creating map
+
+            rooms = new Room[,]
+            {
+                {
+                    new Room("Entrance", false, true, true, false, false, false),
+                    new Room("Master Bedroom", false, false, false, true, false, false),
+                    new Room("Bathroom", false, true, true, false, false, false),
+                    new Room("Exit", false, false, false, true, false, true)
+                },
+                {
+                    new Room("Library", true, false, true, false, false, false),
+                    new Room("Store Room", false, true, false, true, false, false),
+                    new Room("Janitor Closet", true, false, true, false, false, false),
+                    new Room("Staircase", false, true, false, true, false, false)
+                },
+                {
+                    new Room("Child's Bedroom", false, true, true, false, false, false),
+                    new Room("Shrine", true, false, true, true, false, false),
+                    new Room("Hallway", false, false, true, true, false, false),
+                    new Room("Conservatory", true, false, false, true, false, false)
+                },
+                {
+                    new Room("Court Hall", true, false, true, false, false, false),
+                    new Room("Treasure Room", false, false, true, true, true, false),
+                    new Room("Labatory", false, false, true, true, false, false),
+                    new Room("Garage", false, false, false, true, false, false)
+                }
+            };
+
+            // Start of game instructions
+            Console.WriteLine("Start");
+
+            // Set Starting values
+            xpos = 0;
+            ypos = 0;
+            currentRoom = rooms[xpos, ypos];
+            bool hasKey = false;
+            char input;
+
+            // Loop until complete
+            do
+            {
+                // Display room infomation
+                DisplayRoom();
+
+                // Ask for direction of move
+                Console.Write("Enter direction: ");
+                input = Char.Parse(Console.ReadLine());
+
+                // Move to next room
+                ChangeRoom(input);
+
+                // Check for Key
+                hasKey = currentRoom.Key;
+                if(hasKey) Console.WriteLine("You have found the key");
+            }
+            while (!hasKey || !currentRoom.End);
+
+            // Display end of game information
+            Console.WriteLine("Finish");
+        }
+
+        static void ChangeRoom(char direction)
+        {
+            // Change by dirrection
+            if (direction == 'n' && currentRoom.North)
+                ypos--;
+
+            else if (direction == 's' && currentRoom.South)
+                ypos++;
+
+            else if (direction == 'e' && currentRoom.East)
+                xpos++;
+
+            else if (direction == 'w' && currentRoom.West)
+                xpos--;
+
+            // Not valid direction
+            else
+                Console.WriteLine("You can't move that way");
+
+            // Set the new current Room
+            currentRoom = rooms[ypos, xpos];
+        }
+
+        static void DisplayRoom()
+        {
+            // Loop through array
+            for(int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    // Add x when current room
+                    Console.Write("[{0}]", rooms[i, j] == currentRoom ? 'x' : ' ');
+                }
+                Console.Write('\n');
+            }
+        }
+    }
+}
---
+++ RoomEscape/RoomEscape/Room.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomEscape
+{
+    class Room
+    {
+        string name;
+        bool north;
+        bool south;
+        bool east;
+        bool west;
+        bool key;
+        bool end;
+
+        public string Name { get => name; set => name = value; }
+        public bool North { get => north; set => north = value; }
+        public bool South { get => south; set => south = value; }
+        public bool East { get => east; set => east = value; }
+        public bool West { get => west; set => west = value; }
+        public bool Key { get => key; set => key = value; }
+        public bool End { get => end; set => end = value; }
+
+        public Room(string _name, bool _north, bool _south, bool _east, bool _west, bool _key, bool _end)
+        {
+            Name = _name;
+            North = _north;
+            South = _south;
+            East = _east;
+            West = _west;
+            Key = _key;
+            End = _end;
+        }
+    }
+}
